bf8c1fa5ab47adb5031d5ef012e41fc7268de20e
Angel Ortiz <ang3lm4rioortiz@gmail.com>
2023-04-16 16:51:34 -0600

    domingo 162251Z

--- src/ZernikeApprox.jl
+++ src/ZernikeApprox.jl
@@ -2,7 +2,7 @@ module ZernikeApprox
 using LinearAlgebra, Statistics, Random
 using Plots
 using Flux, BSON
-export greet
+export greet, zernike_first_pol
 include("main.jl")
 include("radial.jl")
 
--- src/main.jl
+++ src/main.jl
@@ -10,15 +10,16 @@ function greet(s::String)
     s
 end
 function zernike_first_cart(n::Int, m::Int, x::AbstractFloat, y::AbstractFloat)
-    @assert 0 ≤ m && m ≤ n "m ∉ [0, n]."
-    @assert mod(n - abs(m), 2) ≠ 0 "n - abs(m) should be an even number."
+    @assert abs(m) ≤ n "m ∉ [-n, n]."
+    @assert mod(n - abs(m), 2) ≠ 0.0 "n - abs(m) should be an even number."
+    @assert √(x^2 + y^2) ≤ one(x) "Outside the unit circle."
 
-    ρ = sqrt(x^2 + y^2)
+    ρ = √(x^2 + y^2)
     θ = atan(y, x)
     if abs(m) == zero(m)
-        norm_f = sqrt(n + one(n))
+        norm_f = √(n + one(n))
     else
-        norm_f = sqrt(oftype(n, 2)*(n + one(n)))
+        norm_f = √(oftype(n, 2)*(n + one(n)))
     end
     if m ≥ zero(m)  
         ang_f = cos(m*θ)
@@ -29,6 +30,7 @@ function zernike_first_cart(n::Int, m::Int, x::AbstractFloat, y::AbstractFloat)
     return norm_f*rad_poly*ang_f
 end
 function zernike_first_pol(n::Int, m::Int, ρ::AbstractFloat, θ::AbstractFloat)
+    @assert ρ ≤ one(ρ)
     @assert 0 ≤ m && m ≤ n "m ∉ [0, n]."
     @assert mod(n - abs(m), 2) ≠ 0 "n - abs(m) should be an even number."
 
