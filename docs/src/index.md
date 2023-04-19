52ec3bb442f7b8c238f7e800164b1e9939e4d47c
Angel Ortiz <ang3lm4rioortiz@gmail.com>
2023-04-19 00:59:35 -0600

    martes 190659Z - massive CI changes

--- docs/src/index.md
+++ docs/src/index.md
@@ -1,3 +1,8 @@
 # ZernikeApprox.jl
 
 Documentation for ZernikeApprox.jl
+
+```@docs
+zernike_pol(n, m, rho, theta)
+zernike_rec(n, m, rho, theta)
+```
--- src/ZernikeApprox.jl
+++ src/ZernikeApprox.jl
@@ -1,5 +1,5 @@
 module ZernikeApprox
-export greet
+export greet, zernike_pol, zernike_rec
 include("deps.jl")
 include("main.jl")
 include("radial.jl")
