486fec1848facad5b2e02de01df74eddf497f17f
Angel Ortiz <ang3lm4rioortiz@gmail.com>
2023-04-24 21:36:43 -0600

    lunes 250336Z - beginning of NN, CI correction

--- src/ZernikeApprox.jl
+++ src/ZernikeApprox.jl
@@ -1,6 +1,6 @@
 module ZernikeApprox
 export greet, zernike_pol, zernike_rec
-export model_create. model_train!, model_train_CPU!, zernike_approx, zernike_approx_gpu
+export model_create, model_train!, model_train_CPU!, zernike_approx, zernike_approx_gpu
 include("deps.jl")
 include("main.jl")
 include("radial.jl")
