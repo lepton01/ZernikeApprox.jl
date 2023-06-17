d38158d90fa00fd05bb4dd6c3b1716573eeb862c
Angel Ortiz <ang3lm4rioortiz@gmail.com>
2023-06-16 20:25:25 -0600

    160225Z - huge changes... CNN

--- src/deps.jl
+++ src/deps.jl
@@ -1,3 +1,3 @@
 using LinearAlgebra, Statistics, Random
-using Flux, BSON, ZernikePolynomials, CUDA
+using Flux, BSON, ZernikePolynomials, CUDA, Optimisers, ProgressMeter
 using Flux: mae
--- src/train2.jl
+++ src/train2.jl
@@ -1,25 +1,18 @@
 """
-    coefftrain!(train_data, test_data, name, ep; bat=false, bs=2)
+    coefftrain!(train_data, name; ep=100, bs=2)
 
 Train the `name` model to extract the Zernike coefficients and return the percentage of succesful testset approximation.
 
 bat is `Bool` for optio of using batching data ingestion (faster training) and bs is `Int` for batch size.
 """
-function coefftrain!(DATA_train, DATA_test, model_name::String, ep::Integer=5_000; bat=false, bs=2)::Float32
-    X_train = Flux.flatten(DATA_train[1])
-    Y_train = DATA_train[2]
-    X_test = Flux.flatten(DATA_test[1])
-    Y_test = DATA_test[2]
-    if bat==false
-        train_SET = Flux.DataLoader((X_train, Y_train), batchsize=bs, parallel=true, shuffle=true) |> gpu
-    else
-        train_SET = [(X_train, Y_train)] |> gpu
-    end
+function coefftrain!(DATA_train, model_name::String; ep::Integer=100, bs=2)
+    X_train, Y_train = DATA_train
+    CUDA.@time train_SET = Flux.DataLoader((X_train, Y_train) |> gpu, batchsize=bs, parallel=true, shuffle=true)
     BSON.@load model_name * ".bson" model
     model = model |> gpu
-    opt = Flux.setup(Flux.Adam(), model)
+    opt = Optimisers.setup(Optimisers.Adam(), model)
     #loss_log = Float32[]
-    for i in 1:ep
+    @showprogress 1 "Training..." for i in 1:ep::Int
         losses = Float32[]
         for data in train_SET
             input, label = data
@@ -37,7 +30,7 @@ function coefftrain!(DATA_train, DATA_test, model_name::String, ep::Integer=5_00
                 println("Epoch = $i. Training loss = $l2")
             end
         else
-            if rem(i, 100) == 0
+            if rem(i, 10) == 0
                 println("Epoch = $i. Training loss = $l2")
             end
         end
@@ -50,10 +43,31 @@ function coefftrain!(DATA_train, DATA_test, model_name::String, ep::Integer=5_00
         end
         =#
     end
-    #X_test::Array{Float32, 2} = vcat(N_test', M_test', Ρ_test', Θ_test')
-    Y_hat = model(X_test |> gpu) |> cpu
+    #Y_hat1 = model(X_train |> gpu) |> cpu
+    #Y_hat2 = model(X_test |> gpu) |> cpu
     model = model |> cpu
     BSON.@save model_name * ".bson" model
     CUDA.reclaim()
-    return mean(isapprox.(Y_hat, Y_test; atol=0.015)) * 100
+    return#accuracy(DATA_train, DATA_test, model_name)
+end
+function accuracygpu(A, B, name)
+    BSON.@load name * ".bson" model
+    model = model |> gpu
+    X1, Y1 = A
+    X2, Y2 = B
+    Y_tr_r = model(X1 |> gpu) |> cpu
+    Y_te_r = model(X2 |> gpu) |> cpu
+    a = mean(isapprox.(Y_tr_r, Y1; atol=0.015)) * 100
+    b = mean(isapprox.(Y_te_r, Y2; atol=0.015)) * 100
+    return a, b
+end
+function accuracycpu(A, B, name)
+    BSON.@load name * ".bson" model
+    X1, Y1 = A
+    X2, Y2 = B
+    Y_tr_r = model(X1 .|> Float32)
+    Y_te_r = model(X2 .|> Float32)
+    a = mean(isapprox.(Y_tr_r, Y1; atol=0.015)) * 100
+    b = mean(isapprox.(Y_te_r, Y2; atol=0.015)) * 100
+    return a, b
 end
